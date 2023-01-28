d500cdc568afd7cec1255edc7584fe746d2e0791
Yeseong Jeon <cjhj131@gmail.com>
2023-01-27 22:30:33 -0800

    Background First commit

---
+++ background.js
@@ -0,0 +1,118 @@
+class BackgroundHill { 
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/backgroundHill.png");
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0, 3800, 2000, 0, 192, 12000, 7000);
+                                        // x, y,        frameLocation,framesize x , y, 
+    }
+};
+
+class TreesAndWaterfall { 
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/treesAndWaterfall.png");
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0, 3800, 2000, 0, 360, 12000, 7000);
+                                        // x, y,        frameLocation,framesize x , y, 
+    }
+};
+
+class Cloud1 { 
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/cloud1.png");
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0, 700, 200, 0, 0, 1400, 600);
+                                        // x, y,        frameLocation,framesize x , y, 
+    }
+};
+class Cloud2 { 
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/cloud2.png");
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0, 700, 200, 0, 96, 1400, 600);
+                                        // x, y,        frameLocation,framesize x , y, 
+    }
+};
+
+class Cloud3 { 
+    constructor(game, x, y) {
+        Object.assign(this, { game, x, y });
+
+        this.spritesheet = ASSET_MANAGER.getAsset("./sprites/cloud3.png");
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.spritesheet, 0, 0, 700, 200, 0, 144, 1400, 600);
+                                        // x, y,        frameLocation,framesize x , y, 
+    }
+};
+
+class Water {
+    constructor(game) {
+        this.game = game;
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/water.png"), 0, 0, 400, 105, 8, 0.2);
+                                                                //xStart, yStart, width, height, frameCount, frameDuration
+    };
+
+    update() {
+
+    };
+
+    drawMinimap(ctx, mmX, mmY) {
+    }
+
+    draw(ctx) {
+        this.animator.drawFrame(this.game.clockTick, ctx, 0, 501, 3);
+       
+    };
+}
--- main.js
+++ main.js
@@ -2,9 +2,25 @@ const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
 
+ASSET_MANAGER.queueDownload("./sprites/backgroundHill.png");
+ASSET_MANAGER.queueDownload("./sprites/cloud1.png");
+ASSET_MANAGER.queueDownload("./sprites/cloud2.png");
+ASSET_MANAGER.queueDownload("./sprites/cloud3.png");
+ASSET_MANAGER.queueDownload("./sprites/water.png");
+ASSET_MANAGER.queueDownload("./sprites/treesAndWaterfall.png");
+
+
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
+	ctx.imageSmoothingEnabled = false;
+
+	gameEngine.addEntity(new TreesAndWaterfall(gameEngine));
+	gameEngine.addEntity(new BackgroundHill(gameEngine));
+	gameEngine.addEntity(new Cloud1(gameEngine));
+	gameEngine.addEntity(new Cloud2(gameEngine));
+	gameEngine.addEntity(new Cloud3(gameEngine));
+	gameEngine.addEntity(new Water(gameEngine));
 
 	gameEngine.init(ctx);
 
